e4c8ebff6cedb79f1ec665cfffc2a47490df9be9
vinhveer <63202399+vinhveer@users.noreply.github.com>
2023-12-26 13:21:09 +0700

    thirdcommit

--- ToolNTU/script2.py
+++ ToolNTU/script2.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 # Đọc thông tin tài khoản từ file
 username = "64132989"
-password = "Vinhveer0986209261"
+password = "<Pass>"
 
 # Khởi tạo trình duyệt
 driver = webdriver.Chrome()
